edd7fccff6d21280417f591ba806c8666dcf4363
sutaking <zhaofeng.vip@gmail.com>
2018-10-26 17:10:18 +0800

    update path

--- dev.js
+++ dev.js
@@ -1,7 +1,18 @@
 const cli = require('tizen-tv-dev-cli');
 const path = require('path');
 
-console.log(cli)
-console.log(path.normalize('/app'));
+//console.log(cli)
+console.log(path.normalize(`${__dirname}/app`));
 
-//cli.buildPackage.handleCommand('./app');
+cli.buildPackage.handleCommand(`${__dirname}/app`);
+
+
+
+
+
+
+
+
+
+
+
