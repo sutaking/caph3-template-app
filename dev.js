86ad0dcc4845acfa7b89aa1cd5be6683d2740440
sutaking <zhaofeng.vip@gmail.com>
2018-10-27 10:56:41 +0800

    add launch method

--- dev.js
+++ dev.js
@@ -4,9 +4,9 @@ const path = require('path');
 //console.log(cli)
 console.log(path.normalize(`${__dirname}/app`));
 
-cli.buildPackage.handleCommand(`${__dirname}/app`);
+//cli.buildPackage.handleCommand(`${__dirname}/app`);
 
-
+cli.launchTarget.handleCommand('109.123.121.93', `${__dirname}/app`)
 
 
 
