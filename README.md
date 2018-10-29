c69541aad7713a58fcf94273b7969d90f9999dbe
sutaking <zhaofeng.vip@gmail.com>
2018-10-29 21:02:38 +0800

    update status

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # caph3-template-app
 
 ## Todo-list
--   update api sdb install & launch of tizen-tv-dev-cli, now only work tpk type.
--   add args to npm script
+-   ~~update api sdb install & launch of tizen-tv-dev-cli, now only work tpk type.~~
+-   ~~add args to npm script~~
 -   caph3 install with npm package
 -   create-tizen-caph3-app, npm package.
--- dev.js
+++ dev.js
@@ -1,16 +1,13 @@
 const path = require('path');
 const minimist = require('minimist');
 
-
 const cli = require('tizen-tv-dev-cli');
 
 const args = minimist(process.argv.slice(2));
 
-
 // const value
 const argv = args._[0];
 const appPath = path.normalize(`${__dirname}/app`);
-
 
 (function(){
     console.log(`arg: ${argv}`);
@@ -18,10 +15,10 @@ const appPath = path.normalize(`${__dirname}/app`);
 
     switch(argv) {
         case 'build':
-            cli.buildPackage.handleCommand(appPath);
+            cli.buildPackage.handleCommand(appPath, '.wgt');
             break;
         case 'launch':
-            cli.launchTarget.handleCommand('109.123.121.93', appPath)
+            cli.launchTarget.handleCommand('109.123.121.93', appPath, '.wgt')
             break;
         default:
             break;
